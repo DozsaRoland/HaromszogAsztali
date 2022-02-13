accccf440ac07c5c6a8d949a9e7f6d4a1b6b2550
DozsaRoland <dozsa.roland@njit.hu>
2022-02-13 20:03:34 +0100

    Alapok

---
+++ HaromszogAsztali/3szogalapok.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaromszogAsztali
+{
+   public class _3szogalapok
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double c { get; set; }
+
+
+        public _3szogalapok(double _a, double _b, double _c)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+
+        public bool szerkeszt()
+        {
+            if (a + b > c && a + c > b && b + c > a) return true;
+            throw new _3szogkivetel();
+        }
+
+        public double K()
+        {
+            return a + b + c;
+        }
+
+        public double T()
+        {
+            double s = K() / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+
+    
+}
